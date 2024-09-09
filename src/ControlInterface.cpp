0105a65b9ad4d23990b30e8d6db784609d352be3
Spacedays <44454593+Spacedays@users.noreply.github.com>
2024-09-09 00:41:30 -0400

    Moves Serial parsing to a function

--- src/ControlInterface.cpp
+++ src/ControlInterface.cpp
@@ -10,159 +10,234 @@
 // #define ACKPACKET
 // #define ECHO_MSGPACK // #DEBUG - echo msgpack messages as string
 
+#define DEBUG
+
+#ifdef DEBUG
+#define D(x) x
+#else
+#define D(x)
+#endif // Debug macro
+
 void CalcSteerCenter(int16_t *d, int16_t *h, int joyx, int joyy)
 {
-    // sgn(joyx)*STEERCTR_D_MIN + STEERCTR_SCALING * atan2(JOY_MAX, joyx*M_PI_2);
-    *d = sgn(joyx)*STEERCTR_D_MIN + STEERCTR_SCALING * tan(joyx*M_PI_2 / JOY_MAX + M_PI_2);
-    // hmax = (abs(d) - STEERCTR_D_MIN)*tan(STEERANGLE_MAX_RAD)
-    *h = joyy / JOY_MAX * (abs(*d) - STEERCTR_D_MIN)*tan(STEERANGLE_MAX_RAD);
+	// sgn(joyx)*STEERCTR_D_MIN + STEERCTR_SCALING * atan2(JOY_MAX, joyx*M_PI_2);
+	*d = sgn(joyx) * STEERCTR_D_MIN + STEERCTR_SCALING * tan(joyx * M_PI_2 / JOY_MAX + M_PI_2);
+	// hmax = (abs(d) - STEERCTR_D_MIN)*tan(STEERANGLE_MAX_RAD)
+	*h = joyy / JOY_MAX * (abs(*d) - STEERCTR_D_MIN) * tan(STEERANGLE_MAX_RAD);
 }
 
 void CalcMotionVector(MotionVector *mvec, int joyx, int joyy, int throttle)
 {
-    int16_t d, h;
-    CalcSteerCenter(&d, &h, joyx, joyy);
-    int SCdist[4] = {sqrt(sq(SCDY - h)+sq(-SCDX - d)), sqrt(sq(SCDY - h)+sq(SCDX - d)), sqrt(sq(-SCDY - h)+sq(-SCDX - d)), sqrt(sq(-SCDY - h)+sq(SCDX - d))};
-    mvec->aFL = atan2(SCDY - h, -SCDX - d);
-    mvec->aFR = atan2(SCDY - h, SCDX - d);
-    mvec->aBL = atan2(-SCDY - h, -SCDX - d);
-    mvec->aBR = atan2(-SCDY - h, SCDX - d);
-    int m = max(max(SCdist[0], SCdist[1]), max(SCdist[2], SCdist[3]));
+	int16_t d, h;
+	CalcSteerCenter(&d, &h, joyx, joyy);
+	int SCdist[4] = {sqrt(sq(SCDY - h) + sq(-SCDX - d)), sqrt(sq(SCDY - h) + sq(SCDX - d)), sqrt(sq(-SCDY - h) + sq(-SCDX - d)), sqrt(sq(-SCDY - h) + sq(SCDX - d))};
+	mvec->aFL = atan2(SCDY - h, -SCDX - d);
+	mvec->aFR = atan2(SCDY - h, SCDX - d);
+	mvec->aBL = atan2(-SCDY - h, -SCDX - d);
+	mvec->aBR = atan2(-SCDY - h, SCDX - d);
+	int m = max(max(SCdist[0], SCdist[1]), max(SCdist[2], SCdist[3]));
 
-    mvec->vFL = SCdist[0]/m*throttle;
-    mvec->vFR = SCdist[1]/m*throttle;
-    mvec->vBL = SCdist[2]/m*throttle;
-    mvec->vBR = SCdist[3]/m*throttle;
+	mvec->vFL = SCdist[0] / m * throttle;
+	mvec->vFR = SCdist[1] / m * throttle;
+	mvec->vBL = SCdist[2] / m * throttle;
+	mvec->vBR = SCdist[3] / m * throttle;
 }
 
+/* --- MsgPackHandler --- */
+PacketHandler::PacketHandler(ControlPacketCallback cmdcb, StrPacketCallback strcb)
+{
+	CmdCallback = cmdcb;
+	StrCallback = strcb;
+}
+
+int PacketHandler::ParseSerial(const u8_t cbuff_start_idx = 0)
+{
+
+	// -1: failed to parse
+	// -2: message length header of 0
+	// -3: unterminated string
+
+	char c = '\0';
+
+	bool partial;
+	bool moredata;
+
+	u8_t i = cbuff_start_idx;
+	u8_t j = 0;
+	char lenbuf[5]; // up to 99999 bytes?
+	uint msglen = 0;
+
+	// Read characters until '\n'. MsgPack messages will be preceeded by "\n~[msg_len]\n", then followed by the binary data
+	while (Serial.available() && c != '\n' && i < 64)
+	{
+		c = Serial.read();
+		memcpy(&cbuff[i], &c, 1);
+		i++;
+	}
+
+	// Return if no data
+	if (i == 0)
+		return 0;
+
+	// Received String - do something with it
+	D(Serial.printf("-\n-Received Message w/ len %d: '%s'\n", i, cbuff);)
+	if (c == '\n')
+	{
+		if (Serial.available() > 0)
+		{
+			if (Serial.peek() == LEN_SEP) // MsgPack message received; Process & handle message
+			{
+				c = Serial.read(); // consume LEN_SEP
+
+				// Retrieve length of msgpack msg
+				j = 0;
+				c = Serial.read();
+				while (c != LEN_SEP && j < 5)
+				{
+					lenbuf[j] = c;
+					c = Serial.read();
+					j++;
+				}
+				msglen = strtol(lenbuf, 0, 0);
+				memset(lenbuf, 0, sizeof(lenbuf));
+
+				if (!msglen)
+					return -2;
+				j = Serial.readBytes(msgdata, msglen);
+				if (j < msglen)
+				{
+					Serial.printf("!Incomplete Msgpack; waiting on %dB...", msglen - j);
+					Serial.readBytes(msgdata, msglen - j);
+				}
+				// #DEBUG - data prints
+				D(Serial.print("Data: ");)
+				D(Serial.write(msgdata, msglen);)
+				D(Serial.printf("\nLen: %d\n", msglen);)
+				if (!unpacker.feed(msgdata, msglen))
+					return -1; // No data to unpack
+				// if (unpacker.isArray()) // #TODO: define EXT array type or look at headers to handle various control packets
+				if (unpacker.deserialize(cmd))
+				{
+#ifdef ACKPACKET
+					Serial.println("ACK\r");
+#endif
+					CmdCallback(&cmd);
+#ifdef ECHO_MSGPACK
+					PrintPacket(&cmd);
+#endif
+					return 0;
+				}
+				unpacker.clear();
+				Serial.println(F("\r\n!Failed to deserialize!\r"));
+				return -2;
+			}
+			else // Received terminated string message, more data is still in rx queue
+				partial = false;
+
+			moredata = true;
+		}
+		partial = false; // Since no more RX characters, assume terminated string
+		moredata = false;
+	}
+	else // Received unterminated string
+	{
+		// #TODO: Reached end of buffer while reading string; process part of string message
+		Serial.print(F("\r\n!Received unterminated string with length "));
+		Serial.println(i);
+		if (i >= 63)
+			moredata = true;
+		else
+			partial = true;
+	}
+
+	if (moredata) // recurse ParseSerial if more rx bytes to handle
+	{
+		if (i >= 63)
+		{
+			StrCallback(cbuff, i, false, true);
+			D(Serial.println(F("\r\n!Recursing ParseSerial - buffer full"));)
+			return ParseSerial();
+		}
+		else
+		{
+			D(Serial.println(F("\r\n!Recursing ParseSerial - More data available"));)
+			return ParseSerial(i);
+		}
+	}
+	StrCallback(cbuff, i, partial, false);
+	return 0;
+}
+
+void PacketHandler::SendPacket(const ControlPacket *cmd)
+{
+	// packer.to_array(cmd.a, cmd.b, cmd.ljx, cmd.ljy, cmd.s);
+	packer.serialize(*cmd);
+	Serial.print(PACKETDELIM); // F("\n~"));
+	Serial.print(packer.size());
+	Serial.print(LEN_SEP); //'~');
+	Serial.write((char *)packer.data(), packer.size());
+	packer.clear();
+	Serial.println(); // F("\nFinished writing control packet!"));
+}
 
 /* --- Test Code for controls --- */
+
 #ifdef CONTROLTEST
+#warning BUILDING CONTROLTEST
 
-MsgPack::Unpacker unpacker;
-MsgPack::Packer packer;
 
-ControlPacket cmd = {false, false, 125, 126, "Hello, World!"};
-char cbuff[64];
-char c = '\0';
-void setup()
+void PrintStrMsg(char *msg, uint strlen, bool partial, bool overflow)
 {
-    Serial.begin(115200);
-    delay(2000);
-    Serial.println(F("Setup complete."));
+	Serial.write(msg, strlen);
+	Serial.println();
+	if (partial)
+		Serial.print("!Partial! ");
+	if (overflow)
+		Serial.print("!Overflow!");
 }
 
+// void PrintSteerCenter()
+
+PacketHandler pkt(PrintPacket, PrintStrMsg);
+
+u16_t *d;
+u16_t *h;
+
+uint blinkduration = 300;
+unsigned long lastupdate = 0;
+long blinktime = 0;
 bool blinkstate;
-bool read_success;
-byte msgdata[128]; // #TODO: see if this is enough?
-u8_t i = 0;
-u8_t j = 0;
-char lenbuf[5];     // up to 99999 bytes?
-uint msglen = 0;
-u8_t blinkduration = 200;
-u32_t lastupdate = 0;
-u32_t blinktime = 0;
+
+void setup()
+{
+	Serial.begin(115200);
+	delay(2000);
+	pinMode(LED_BUILTIN, OUTPUT);
+	digitalWrite(LED_BUILTIN, HIGH);
+	Serial.println(F("Setup complete."));
+}
+
 void loop()
 {
-    // Read characters until '\n'. MsgPack messages will be preceeded by "\n~[msg_len]\n", then followed by the binary data
-    while (Serial.available() && c != '\n' && i < 64)
-    {
-        c = Serial.read();
-        memcpy(&cbuff[i], &c, 1);
-        i++;
-    }
-    // Received data
-    if (i > 0)
-    {
-        blinkduration = (blinkduration == 200 ? 500 : 200);
-        // Received String - do something with it
-        // Serial.print("-\n-Received Message: '");     // #DEBUG msg print
-        // Serial.print(cbuff);
-        // Serial.print("'");
-        i = 0;
-        if (c == '\n')
-        {
-            c = Serial.read();
-            if (c == '~')
-            {
-                j = 0;
-                c = Serial.read();
-                while (c != '~' && j < 5)
-                {
-                    lenbuf[j] = c;
-                    c = Serial.read();
-                    j++;
-                }
-                msglen = strtol(lenbuf, 0, 0);
-                // Serial.printf("Rx Len: %d\n", msglen);   // #DEBUG rx length
-                memset(lenbuf, 0, sizeof(lenbuf));
+	pkt.ParseSerial();
 
-                if (!msglen)
-                    return;
-                j = Serial.readBytes(msgdata, msglen);
-                if (j < msglen)
-                {
-                    Serial.printf("Incomplete Msgpack; waiting on %dB...", msglen - j);
-                    Serial.readBytes(msgdata, msglen - j);
-                }
-                // Serial.print("Data: ");          //#DEBUG - data prints
-                // Serial.write(msgdata, msglen);
-                // Serial.printf("\nLen: %d\n", msglen);
-                if (!unpacker.feed(msgdata, msglen))
-                    return;
-                // if (unpacker.isArray()) // #TODO: define EXT array type for various control packets
-                // Serial.println(F("\r\nUnpacking array to object"));  //#DEBUG
-                if (unpacker.deserialize(cmd))
-                {
-#ifdef ACKPACKET
-                    Serial.println("ACK\r");
-#endif
-#ifdef ECHO_MSGPACK
-                    PrintPacket(&cmd);
-#endif
-                    ;
-                }
-                else
-                    Serial.println(F("\r\nFailed to deserialize!\r"));
-            }
-            else
-            {
-                // Message is a string; Reset and continue reading characters
-                cbuff[i] = c;
-                i++;
-            }
-        }
-        else
-        {
-            // #TODO: Reached end of buffer while reading string; process part of string message
-            Serial.print(F("Received partial string with length "));
-            Serial.println(i);
-            i = 0;
-        }
-    }
-    if (millis() - lastupdate > 5000)
-    {
-        // packer.to_array(cmd.a, cmd.b, cmd.ljx, cmd.ljy, cmd.s);
-        packer.serialize(cmd);
-        Serial.print(PACKETDELIM); // F("\n~"));
-        Serial.print(packer.size());
-        Serial.print(LEN_SEP); //'~');
-            Serial.write((char *)packer.data(), packer.size());
-        packer.clear();
-        Serial.println(); // F("\nFinished writing control packet!"));
-        lastupdate = millis();
-    }
-    if (millis() - blinktime > blinkduration)
-    {
-        digitalWrite(LED_BUILTIN, blinkstate);
-        blinkstate = !blinkstate;
-        blinktime = millis();
-    }
+	if (millis() - lastupdate > 2000)	// #DEBUG Why does this break above 2000?
+	{
+		// packer.to_array(cmd.a, cmd.b, cmd.ljx, cmd.ljy, cmd.s);
+		pkt.SendPacket(&(pkt.cmd));
+		lastupdate = millis();
+	}
+	if (millis() - blinktime > blinkduration)
+	{
+		digitalWrite(LED_BUILTIN, blinkstate);
+		blinkstate = !blinkstate;
+		blinktime = millis();
+	}
 }
 
 void PrintPacket(ControlPacket *cmd)
 {
-    Serial.printf("CP: %d %d %d %d %s\n", cmd->a, cmd->b, cmd->ljx, cmd->ljy, cmd->s);
+	Serial.printf("CP: %d %d %d %d %s\n", cmd->a, cmd->b, cmd->ljx, cmd->ljy, cmd->s);
 }
 
 #endif
--- src/ControlInterface.h
+++ src/ControlInterface.h
@@ -9,18 +9,18 @@ extern void loop();
 #endif
 
 #define PACKETDELIM "\n~"
-#define LEN_SEP "~"
+#define LEN_SEP '~'
 
 #define JOY_MAX 1024
 
 struct ControlPacket
 {
-    bool a;
-    bool b;
-    int ljx = 125;
-    int ljy = 126;
-    MsgPack::str_t s = "Hello, Pi!";
-    MSGPACK_DEFINE(a, b, ljx, ljy, s); // conversion to array -> [a, b, ljx, ljy, s]
+	bool a;
+	bool b;
+	int ljx = 125;
+	int ljy = 126;
+	MsgPack::str_t s = "Hello, Pi!";
+	MSGPACK_DEFINE(a, b, ljx, ljy, s); // conversion to array -> [a, b, ljx, ljy, s]
 };
 
 void PrintPacket(ControlPacket *cmd);
@@ -40,25 +40,57 @@ const float STEERCTR_SCALING = 1;
 const u8_t SCDX = 114;
 const u8_t SCDY = 141;
 template <typename T> int sgn(T val) {
-    return (T(0) < val) - (val < T(0));
+	return (T(0) < val) - (val < T(0));
 }
 
 struct MotionVector
 {
-    int vFL;
-    int vFR;
-    int vBL;
-    int vBR;
-    int aFL;
-    int aFR;
-    int aBL;
-    int aBR;
-    MSGPACK_DEFINE(vFL, vFR, vBL, vBR, aFL, aFR, aBL, aBR);
+	int vFL;
+	int vFR;
+	int vBL;
+	int vBR;
+	int aFL;
+	int aFR;
+	int aBL;
+	int aBR;
+	MSGPACK_DEFINE(vFL, vFR, vBL, vBR, aFL, aFR, aBL, aBR);
 };
 
+
+void CalcSteerCenter(int16_t *d, int16_t *h, int joyx, int joyy);
 
 void CalcMotionVector(MotionVector *mvec, int joyx, int joyy, int throttle);
 
 // MotionVector ProcessPacket(ControlPacket);
+
+// Callback function which should be user supplied and which will be called for each ControlPacket
+typedef void(*ControlPacketCallback)(ControlPacket *cmd);
+// Callback function for received strings; partial (unterminated message) and overflow (ran out of buffer) are mutually exclusive
+typedef void(*StrPacketCallback)(char *msg, uint strlen, bool partial, bool overflow);
+
+
+class PacketHandler
+{
+	public:
+		PacketHandler(const ControlPacketCallback cmdcb, const StrPacketCallback strcb);   // #TODO: pass serial instance
+		// ~MsgPackHandler();   // Necessary if dynamic allocation
+		int ParseSerial(u8_t cbuff_start_idx);
+		void SetCmdCallback(const ControlPacketCallback callback);
+		void SetStrCallback(const StrPacketCallback callback);
+		ControlPacketCallback CmdCallback;
+		StrPacketCallback StrCallback;
+		
+		void SendPacket(const ControlPacket *cmd);
+		
+		MsgPack::Unpacker unpacker;
+		MsgPack::Packer packer;
+
+		ControlPacket cmd = {false, false, 125, 126, "Hello, World!"}; // #TODO: move to class member
+		ControlPacket prevCmd;
+	private:
+		byte msgdata[128]; // #TODO: see if this is enough?
+		char cbuff[64];
+
+};
 
 #endif
